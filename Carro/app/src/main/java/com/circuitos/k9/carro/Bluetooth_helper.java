dc78f6abe6d1a62722c51af89684e5d235e1555d
Konrad <konri9@gmail.com>
2015-11-17 00:43:09 -0600

    Blablalbla nuevo brete bla bla

--- Carro/app/src/main/java/com/circuitos/k9/carro/Bluetooth_helper.java
+++
@@ -1,7 +0,0 @@
-package com.circuitos.k9.carro;
-
-/**
- * Created by k9 on 13/11/15.
- */
-public class Bluetooth_helper {
-}
--- Carro/app/src/main/java/com/circuitos/k9/carro/MainActivity.java
+++ Carro/app/src/main/java/com/circuitos/k9/carro/MainActivity.java
@@ -1,12 +1,7 @@
 package com.circuitos.k9.carro;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,45 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void btn_driver(View view)
-    {
-        Intent intent = new Intent(this, Car.class);
-        startActivity(intent);
-    }
-
-    public void btn_credits(View view)
-    {
-        Intent intent = new Intent (this,Credits.class);
-        startActivity(intent);
-    }
-
-    public void btn_clax (View view)
-    {
-        Toast.makeText(this, "PIIIIIIIIII PIIIIIII", Toast.LENGTH_SHORT).show();
-    }
-
-
 }
